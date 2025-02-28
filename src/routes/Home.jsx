58774abf4819499bc4b9fe830e7292b8136c3374
web3senior <atenyun@gmail.com>
2025-02-28 21:13:11 +0330

    Fix user list

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -21,7 +21,7 @@ function Home() {
 
   const getAllEmoji = async () => await contract.methods.getAllEmoji().call()
 
-  const getAllUserReaction = async () => await contract.methods.getAllUserReaction(`0xcec6d2dd0d8d54e137d0d0e5787dd2208e297ecf`).call()
+  const getAllUserReaction = async () => await contract.methods.getAllUserReaction(`${auth.contextAccounts[0]}`).call()
 
   const action = async (e, emoji) => {
     const t = toast.loading(`Waiting for transaction's confirmation`)
@@ -56,53 +56,51 @@ function Home() {
     }
   }
 
-
- const getIPFS = async (CID) => {
-  let requestOptions = {
-    method: 'GET',
-    redirect: 'follow',
+  const getIPFS = async (CID) => {
+    let requestOptions = {
+      method: 'GET',
+      redirect: 'follow',
+    }
+    const response = await fetch(`${import.meta.env.VITE_IPFS_GATEWAY}${CID}`, requestOptions)
+    if (!response.ok) return { result: false } //throw new Response('Failed to get data', { status: 500 })
+    return response.json()
   }
-  const response = await fetch(`${import.meta.env.VITE_IPFS_GATEWAY}${CID}`, requestOptions)
-  if (!response.ok) return { result: false } //throw new Response('Failed to get data', { status: 500 })
-  return response.json()
-}
 
-/**
- * Fetch Universal Profile
- * @param {address} addr
- * @returns
- */
+  /**
+   * Fetch Universal Profile
+   * @param {address} addr
+   * @returns
+   */
 
- const fetchProfile = async (_addr) => {
-  const web3 = new Web3(import.meta.env.VITE_LUKSO_PROVIDER)
-  const LSP0ERC725Contract = new web3.eth.Contract(LSP0ERC725Account.abi, _addr)
-  try {
-    return LSP0ERC725Contract.methods
-      .getData('0x5ef83ad9559033e6e941db7d7c495acdce616347d28e90c7ce47cbfcfcad3bc5')
-      .call()
-      .then(async (data) => {
-        data = data.substring(6, data.length)
-        // console.log(data)
-        //  data ="0x" + data.substring(6)
-        //  console.log(data)
-        // slice the bytes to get its pieces
-        const hashFunction = '0x' + data.slice(0, 8)
-        // console.log(hashFunction)
-        const hash = '0x' + data.slice(76)
-        const url = '0x' + data.slice(76)
-        // console.log(hashFunction, ' | ', hash, ' | ', url)
-        // check if it uses keccak256
-        //  if (hashFunction === '0x6f357c6a') {
-        // download the json file
-        // console.log(`-------------`,web3.utils.hexToUtf8(url).replace('ipfs://', '').replace('://', ''))
-        const json = await getIPFS(web3.utils.hexToUtf8(url).replace('ipfs://', '').replace('://', ''))
-        return json
-      })
-  } catch (error) {
-    console.log(`fetch profile error => `, error)
-    return []
+  const fetchProfile = async (_addr) => {
+    const LSP0ERC725Contract = new web3.eth.Contract(LSP0ERC725Account.abi, _addr)
+    try {
+      return LSP0ERC725Contract.methods
+        .getData('0x5ef83ad9559033e6e941db7d7c495acdce616347d28e90c7ce47cbfcfcad3bc5')
+        .call()
+        .then(async (data) => {
+          data = data.substring(6, data.length)
+          // console.log(data)
+          //  data ="0x" + data.substring(6)
+          //  console.log(data)
+          // slice the bytes to get its pieces
+          const hashFunction = '0x' + data.slice(0, 8)
+          // console.log(hashFunction)
+          const hash = '0x' + data.slice(76)
+          const url = '0x' + data.slice(76)
+          // console.log(hashFunction, ' | ', hash, ' | ', url)
+          // check if it uses keccak256
+          //  if (hashFunction === '0x6f357c6a') {
+          // download the json file
+          // console.log(`-------------`,web3.utils.hexToUtf8(url).replace('ipfs://', '').replace('://', ''))
+          const json = await getIPFS(web3.utils.hexToUtf8(url).replace('ipfs://', '').replace('://', ''))
+          return json
+        })
+    } catch (error) {
+      console.log(`fetch profile error => `, error)
+      return []
+    }
   }
-}
 
   useEffect(() => {
     console.clear()
@@ -117,11 +115,12 @@ function Home() {
 
     getAllUserReaction().then(async (res) => {
       console.log(res)
+
       if (res.length > 0) setReact(res)
 
       let responses_with_profile = []
       await Promise.all(
-        res.map((response, i) => {
+        res.map(async (response, i) => {
           return fetchProfile(response.sender).then((profile) => {
             responses_with_profile.push(Object.assign(profile, response))
           })
