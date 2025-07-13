4cd32572976bf6e1a1c94ac80522439955176306
Amir <atenyun@gmail.com>
2025-07-13 04:21:55 +0330

    Remove files

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"/FCTmKjfJFhHTmxkWDSqxKs0M0F52QBKId4UzfDdEik=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
