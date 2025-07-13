095b706dc1269ba4a6ff7d628ba4eb739a378f54
Amir <atenyun@gmail.com>
2025-07-13 04:19:49 +0330

    Remove extra files

--- app/claim/submit/[id]/loading.jsx
+++
@@ -1,6 +0,0 @@
-import {Loading as CustomLoading} from '../../../components/Loading'
-
-export default function Loading() {
-    // Or a custom loading skeleton component
-    return <CustomLoading/>
-  }
